b8d63b33b3c886e9da0c0be4d9ba0de1261e66d2
CJDICE <icechiu@aetheras.io>
2023-08-06 00:22:18 +0800

    update

--- apply_cloud_setting.sh
+++ apply_cloud_setting.sh
@@ -4,9 +4,22 @@
 cpu_info=$(sysctl -n machdep.cpu.brand_string)
 
 if echo "$cpu_info" | grep -qi "Intel"; then
-  echo "$cpu_info"
+  echo "Use \"$cpu_info\" setting"
+    echo "  Overwrite zshrc"
+  cp ~/.zshrc ~/.zshrc.bak
+  cp ./.zshrc ~/.zshrc
+  echo "  Overwrite vimrc"
+  cp ~/.vimrc ~/.vimrc.bak
+  cp ./.vimrc ~/.vimrc
+elif echo "$cpu_info" | grep -qi "M1"; then
+  echo "Use \"$cpu_info\" setting"
+  echo "  Overwrite zshrc"
+  cp ~/.zshrc ~/.zshrc.bak
+  cp ./.zshrc_m1 ~/.zshrc
+  echo "  Overwrite vimrc"
+  cp ~/.vimrc ~/.vimrc.bak
+  cp ./.vimrc ~/.vimrc
+  echo "Done"
 else
-  echo "Use $cpu_info setting"
-  mv ~/.zshrc ~/.zshrc.bak
-  cp ./zshrc_m1 ~/.zshrc
+  echo "error: Device Unknown"
 fi
